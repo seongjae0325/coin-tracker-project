bf717dd6998eb63c028dbce3cd64991b6f1278f1
seongjae0325 <90776802+seongjae0325@users.noreply.github.com>
2022-10-05 02:15:43 +0900

    Feat apply loading spinner

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+import LoadingSpinner from "../components/LoadingSpinner";
+
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
   font-size: 35px;
@@ -37,14 +39,29 @@ const Coin = styled.li`
   margin-bottom: 10px;
   a {
     padding: 20px;
+    display: flex;
+    align-items: center;
     transition: color 0.3s ease-in;
-    display: block;
   }
   &:hover {
     a {
       color: ${(props) => props.theme.accentColor};
     }
   }
+`;
+
+const BottomPadding = styled.div`
+  height: 20px;
+  z-index: 1;
+  bottom: 0;
+  position: sticky;
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const Img = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -59,9 +76,12 @@ interface CoinInterface {
 
 function Coins() {
   const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setIsLoading] = useState(true);
+
   const geCoinData = async () => {
     const response = await axios.get("https://api.coinpaprika.com/v1/coins");
-    setCoins(response.data);
+    setCoins(response.data.slice(0, 200));
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -69,18 +89,30 @@ function Coins() {
   }, []);
 
   return (
-    <Container>
-      <Header>
-        <Title>코인 리스트</Title>
-      </Header>
-      <CoinList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={coin.id}>{coin.name}</Link>
-          </Coin>
-        ))}
-      </CoinList>
-    </Container>
+    <>
+      <Container>
+        <Header>
+          <Title>코인 리스트</Title>
+        </Header>
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <CoinList>
+            {coins.map((coin) => (
+              <Coin key={coin.id}>
+                <Link to={coin.id}>
+                  <Img
+                    src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                  />
+                  {coin.name}
+                </Link>
+              </Coin>
+            ))}
+          </CoinList>
+        )}
+      </Container>
+      <BottomPadding />
+    </>
   );
 }
 export default Coins;
